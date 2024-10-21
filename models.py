07e3e1c42c55465737f720a843b35fdac2eeb2f2
lucamodica <modica@chalmers.se>
2024-10-21 10:18:47 +0200

    🎈 perf(model training): partial setup for training multiple gnn archs

--- models.py
+++ models.py
@@ -1,10 +1,18 @@
 import torch
 from torch.functional import F
-from torch_geometric.nn import GCNConv, GATConv, global_mean_pool
+from torch_geometric.nn import GCNConv, GATConv, GINConv, SAGEConv
 import torch.nn as nn
 
+# base GNN class
+class GNN(torch.nn.Module):
+    def __init__(self):
+        super(GNN, self).__init__()
+        pass
 
-class GCN(torch.nn.Module):
+    def forward(self, x, edge_index):
+        pass
+    
+class GCN(GNN):
     def __init__(self, in_channels, hidden_channels, out_channels):
         super(GCN, self).__init__()
         self.conv1 = GCNConv(in_channels, hidden_channels)
@@ -16,7 +24,7 @@ class GCN(torch.nn.Module):
             nn.Linear(hidden_channels, out_channels)
         )
 
-    def forward(self, x, edge_index, batch):
+    def forward(self, x, edge_index):
         # node emebdding creation (embedding = h)
         h = self.conv1(x, edge_index)
         h = h.relu()
@@ -25,16 +33,16 @@ class GCN(torch.nn.Module):
         h = self.conv3(h, edge_index)
 
         # applying a classifier
-        x = F.dropout(h, p=0.2, training=self.training)
-        x = self.lin(x)
-
-        return x
+        # x = F.dropout(h, p=0.2)
+        x = self.fc_layers(h)
+        return F.log_softmax(x, dim=1), h
     
-class GAT(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels):
+
+class GAT(GNN):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_heads):
         super(GAT, self).__init__()
-        self.conv1 = GATConv(in_channels, hidden_channels)
-        self.conv2 = GATConv(hidden_channels, out_channels)
+        self.conv1 = GATConv(in_channels, hidden_channels, heads=num_heads, concat=True)
+        self.conv2 = GATConv(hidden_channels * num_heads, out_channels, heads=num_heads)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index)
@@ -42,3 +50,54 @@ class GAT(torch.nn.Module):
         x = self.conv2(x, edge_index)
         
         return F.log_softmax(x, dim=1)
+    
+class GIN(GNN):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GIN, self).__init__()
+        self.conv1 = GINConv(nn.Sequential(nn.Linear(in_channels, hidden_channels), nn.ReLU(), nn.Linear(hidden_channels, hidden_channels)))
+        self.conv2 = GINConv(nn.Sequential(nn.Linear(hidden_channels, hidden_channels), nn.ReLU(), nn.Linear(hidden_channels, hidden_channels)))
+        self.conv3 = GINConv(nn.Sequential(nn.Linear(hidden_channels, hidden_channels), nn.ReLU(), nn.Linear(hidden_channels, hidden_channels)))
+        self.fc_layers = nn.Sequential(
+            nn.Linear(hidden_channels, hidden_channels),
+            nn.ReLU(),
+            nn.Linear(hidden_channels, out_channels)
+        )
+
+    def forward(self, x, edge_index):
+        # node emebdding creation (embedding = h)
+        h = self.conv1(x, edge_index)
+        h = h.relu()
+        h = self.conv2(h, edge_index)
+        h = h.relu()
+        h = self.conv3(h, edge_index)
+
+        # applying a classifier
+        # x = F.dropout(h, p=0.2)
+        x = self.fc_layers(h)
+        return F.log_softmax(x, dim=1), h
+    
+
+class GraphSAGE(GNN):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GraphSAGE, self).__init__()
+        self.conv1 = SAGEConv(in_channels, hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, hidden_channels)
+        self.conv3 = SAGEConv(hidden_channels, hidden_channels)
+        self.fc_layers = nn.Sequential(
+            nn.Linear(hidden_channels, hidden_channels),
+            nn.ReLU(),
+            nn.Linear(hidden_channels, out_channels)
+        )
+
+    def forward(self, x, edge_index):
+        # node emebdding creation (embedding = h)
+        h = self.conv1(x, edge_index)
+        h = h.relu()
+        h = self.conv2(h, edge_index)
+        h = h.relu()
+        h = self.conv3(h, edge_index)
+
+        # applying a classifier
+        # x = F.dropout(h, p=0.2)
+        x = self.fc_layers(h)
+        return F.log_softmax(x, dim=1), h
