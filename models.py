c3441388c722df3348146542c79eafa489873cb1
lucamodica <modica@chalmers.se>
2024-10-23 20:28:40 +0200

    ✨ feat(exmaple prediction): networkx plotting of the example prediction, with neighbors

--- models.py
+++ models.py
@@ -40,7 +40,7 @@ class GAT(GNN):
     def __init__(self, in_channels, hidden_channels, out_channels, num_heads):
         super(GAT, self).__init__()
         self.conv1 = GATConv(in_channels, hidden_channels, heads=num_heads, concat=True)
-        self.conv2 = GATConv(hidden_channels * num_heads, out_channels, heads=num_heads)
+        self.conv2 = GATConv(hidden_channels * num_heads, out_channels, heads=1, concat=False)
 
     def forward(self, x, edge_index):
         h = self.conv1(x, edge_index)
